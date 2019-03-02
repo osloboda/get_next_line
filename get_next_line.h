08bd7cc43ffeb47c2e132e12919b1b7907ce5ce5
Oleh SLOBODANIUK <osloboda@e1r6p10.unit.ua>
2019-03-02 16:13:20 +0200

    release

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: osloboda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/06 14:50:04 by osloboda          #+#    #+#             */
+/*   Updated: 2018/11/30 19:01:37 by osloboda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 512
+# include "libft/libft.h"
+
+int	get_next_line(const int fd, char **list);
+
+#endif
